31f40f17b7b61756b6b86b8cfb1c35fca89d2dd0
xXLeopoldoXx <britoleopoldo02@gmail.com>
2025-12-17 23:50:42 -0500

    Resuelto conflicto de merge: agregado ActivityIndicator a imports de Round.tsx

--- src/screens/Round/Round.tsx
+++ src/screens/Round/Round.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { View, StyleSheet, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, StyleSheet, ScrollView, KeyboardAvoidingView, Platform, ActivityIndicator } from 'react-native';
 import { Text, Button, TextInput, Card, Chip, ProgressBar } from 'react-native-paper';
 import { ScreenContainer, AnimatedEmoji } from '../../components';
 import { useGame } from '../../game';
